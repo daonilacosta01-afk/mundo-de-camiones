a2a6ab14aae9d9ba66c89a01207ba0bc7374932a
daonilacosta01-afk <daonilacosta01@gmail.com>
2026-05-06 13:52:04 -0400

    Agregar cache para mejorar velocidad

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from datetime import timedelta
 
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = timedelta(days=30)
 
 @app.route('/')
 def index():
